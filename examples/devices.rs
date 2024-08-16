bf4f0c460aa83430e417da6b765c9e5e5c204255
Precious <Kyu@users.noreply.github.com>
2024-08-15 20:22:34 -0500

    Update device listing example to print input devices, and fix formatting

--- examples/devices.rs
+++ examples/devices.rs
@@ -3,6 +3,7 @@ use wasapi::*;
 fn main() {
     initialize_mta().unwrap();
 
+    println!("========== OUTPUT DEVICES ==========");
     println!("Found the following output devices:");
     for device in &DeviceCollection::new(&Direction::Render).unwrap() {
         let dev = device.unwrap();
@@ -13,6 +14,8 @@ fn main() {
             state
         );
     }
+
+    print!("\n");
 
     println!("Default output devices:");
     [Role::Console, Role::Multimedia, Role::Communications]
@@ -27,4 +30,35 @@ fn main() {
                     .unwrap()
             );
         });
+
+    print!("\n\n");
+
+    println!("========== INPUT DEVICES ==========");
+    println!("Found the following input devices:");
+    for device in &DeviceCollection::new(&Direction::Capture).unwrap() {
+        let dev = device.unwrap();
+        let state = &dev.get_state().unwrap();
+        println!(
+            "Device: {:?}. State: {:?}",
+            &dev.get_friendlyname().unwrap(),
+            state
+        );
+    }
+
+    print!("\n");
+
+    println!("Default input devices:");
+    [Role::Console, Role::Multimedia, Role::Communications]
+        .iter()
+        .for_each(|role| {
+            println!(
+                "{:?}: {:?}",
+                role,
+                get_default_device_for_role(&Direction::Capture, role)
+                    .unwrap()
+                    .get_friendlyname()
+                    .unwrap()
+            );
+        });
+
 }
